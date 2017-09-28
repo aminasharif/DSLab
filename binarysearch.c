87d16d302c44d761c40658aea0a7ebe46ee2500c
aminasharif <aiktc@aiktc86>
2017-09-28 12:15:33 +0530

    :quicksort.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,47 +1,34 @@
 #include<stdio.h>
-#define max 100
-
-int binary_search(int a[],int n,int x)
+#define MAX 100
+int binary_search(int a[], int n, int x)
 {
 	int mid=n/2;
 	int high=n-1,low=0;
-	while(high>=low){
+	while(high>=low)
+	{
 		if(a[mid]==x)
-			return 1;
+			return mid;
 		if(x>a[mid])
 			low=mid+1;
 		else
 			high=mid-1;
-		mid=(low+high)/2;
-		}
-		return -1;
+		mid=(high+low)/2;
 	}
-	
-	
+	return -1;
+}
+
 int main()
 {
-	int a[max],i,x,n,index;
-	printf("\nEnter the no of elements of array\n");
+	int a[MAX],n,e,i;
+	printf("Enter the no of elements(max 100):\n");
 	scanf("%d",&n);
-	printf("\nEnter the elements of array\n");
 	for(i=0;i<n;i++)
-	{
-		printf("%d",&a[i]);
-	}
-	printf("\nEnter the element to be searched\n");
-	scanf("%d",&x);
-	index=binary_search(a,n,x);
-	if(index==-1)
-		{
-			printf("Element %d not found",&x);
-		}
+		scanf("%d",&a[i]);
+	printf("Enter the element to be searched\n");
+	scanf("%d",&e);
+	if(binary_search(a,n,e)!=-1)
+		printf("Element found at %d\n",binary_search(a,n,e));
 	else
+		printf("Element not found\n"); 
 	return 0;
-	
-	
-	
-	
-	
-	
-	
-	}
+}
